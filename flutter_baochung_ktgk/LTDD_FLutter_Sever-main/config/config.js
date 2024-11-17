6649de677c01b95ca9bcff4bf10dad1cde81d7d8
ChungCr7 <baochungas3@gmail.com>
2024-11-17 17:16:08 +0700

    update

---
+++ flutter_baochung_ktgk/LTDD_FLutter_Sever-main/config/config.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    // Kết nối với MongoDB
+    await mongoose.connect(
+      "mongodb+srv://baochung_101124:Pbc19402400@fluttercluster.vavrq.mongodb.net/?retryWrites=true&w=majority&appName=flutterCluster",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("Connect successfully!!!");
+  } catch (error) {
+    console.error("Connect failure!!!", error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- flutter_baochung_ktgk/android/app/src/main/kotlin/com/example/flutter_baochung_ktgk/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_baochung_ktgk
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
